2e6d9b7e76c0106aa78dc1873443f7a0ef7fe8b3
RavenInDisguise <monicaalfarop@estudiantec.cr>
2022-09-24 14:45:04 -0600

    Create Sandwich.java

---
+++ src/main/java/com/mycompany/casodecorator/Sandwich.java
@@ -0,0 +1,34 @@
+package com.mycompany.casodecorator;
+
+public class Sandwich {
+ 
+    private String proteina;
+    private double precio;
+    private int tamanno;
+    private String tipo;
+
+    public String getProteina() {
+        return proteina;
+    }
+    public void setProteina(String proteina) {
+        this.proteina = proteina;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public int getTamanno() {
+        return tamanno;
+    }
+    public void setTamanno(int tamanno) {
+        this.tamanno = tamanno;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    } 
+}
